8f27cd6e6d95fdffde2657fd3c75b33866287afd
LaffSteven <steve.laff@gmail.com>
2022-05-28 13:41:30 -0400

    card schema updated

--- models/card.js
+++ models/card.js
@@ -1,8 +1,46 @@
 const mongoose = require('mongoose');
 
 const cardSchema = new mongoose.Schema({
+    name: String,
+    names: [String],
+    manaCost: String,
+    cmc: Number,
+    colors: [String],
+    colorIdentity:[String],
+    type: String,
+    supertypes: [String],
+    types: [String],
+    subtypes: [String],
+    rarity: String,
+    set: String,
+    text: String,
+    artist: String,
+    number: String,
+    power: String,
+    toughness: String,
+    loyalty: String,
+    layout: String,
+    multiverseid: Number,
+    imageUrl: String,
+    rulings: [
+        {
+            date: Date,
+            text: String
+        }
+    ],
+    foreignName:[
+        {
+            name: String,
+            language: String,
+            multiverseid: Number
+        }
+    ],
+    printings: [String],
+    originalText: String,
+    originaltype: String,
+    id: String
 
-})
+});
 
 const Card = mongoose.model('Card', cardSchema);
 
