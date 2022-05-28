d3c93338d6337395468e5c741a383324f95eecc5
LaffSteven <steve.laff@gmail.com>
2022-05-28 11:01:43 -0400

    created router files

---
+++ controllers/cards.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const cardsRouter = express.Router();
+const Card = require('../models/card.js')
+
+
+module.exports = cardsRouter;
---
+++ controllers/decks.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const decksRouter = express.Router();
+const Deck = require('../models/deck.js')
+
+
+module.exports = decksRouter;
