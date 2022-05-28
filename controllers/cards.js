9bf88784e20f6a8fe28784a9fcc223b6d4478951
LaffSteven <steve.laff@gmail.com>
2022-05-28 13:03:00 -0400

    added card seed route

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,9 +2,13 @@ const express = require('express');
 const cardsRouter = express.Router();
 const Card = require('../models/card.js')
 
+cardsRouter.get('/data/seed/import', (req, res) => {
+    res.send(req.body)
+});
+
 
 cardsRouter.get('/', (req, res) => {
     res.send('Card Index Page')
-})
+});
 
 module.exports = cardsRouter;
