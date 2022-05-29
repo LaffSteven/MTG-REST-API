641a4ef68ad81596d36b2d44bb0b2c3b2c32184e
LaffSteven <steve.laff@gmail.com>
2022-05-28 20:48:56 -0400

    test array for api call

--- controllers/cards.js
+++ controllers/cards.js
@@ -24,10 +24,11 @@ const Card = require('../models/card.js')
 
 
 cardsRouter.get('/', (req, res) => {
-    Card.find({}, (err, foundCards) => {
-        console.log(foundCards.length);
-        res.json(foundCards)
-    }).limit(5);
+    // Card.find({}, (err, foundCards) => {
+    //     console.log(foundCards.length);
+    //     res.json(foundCards)
+    // }).limit(5);
+    res.send(["the", "cards", "will", "go", "here"])
 });
 
 module.exports = cardsRouter;
