ba1d6a1c484c772edab33a4dde645ba9a8711302
LaffSteven <steve.laff@gmail.com>
2022-05-30 13:16:45 -0400

    test update

--- controllers/cards.js
+++ controllers/cards.js
@@ -6,27 +6,27 @@ const Card = require('../models/card.js')
 
 
 // only call seed route when working on localhost (already used, please don't run again)
-// let pageIndex = 1;
-// cardsRouter.get('/data/seed/import', (req, res) => {
-//     res.send(`Imported page #${pageIndex}`)
-//     callSeed(pageIndex);
-//     pageIndex++
-// });
-//
-// const callSeed = (pageNum) => {
-//     axios.get(`http://api.magicthegathering.io/v1/cards?page=${pageNum}`)
-//         .then((response) => {
-//             Card.insertMany(response.data.cards, (err, foundCards) => {
-//                 console.log(`Imported Page #${pageNum}`);
-//             });
-//     });
-// }
+let pageIndex = 1;
+router.get('/data/seed/import', (req, res) => {
+    res.send(`Imported page #${pageIndex}`)
+    callSeed(pageIndex);
+    pageIndex++
+});
+
+const callSeed = (pageNum) => {
+    axios.get(`http://api.magicthegathering.io/v1/cards?page=${pageNum}`)
+        .then((response) => {
+            Card.insertMany(response.data.cards, (err, foundCards) => {
+                console.log(`Imported Page #${pageNum}`);
+            });
+    });
+}
 
 
 router.get('/', (req, res) => {
-    Card.find({name: "Bandage"}, (err, foundCards) => {
+    Card.find({}, (err, foundCards) => {
         res.json(foundCards)
-    });
+    }).limit(10);
 });
 
 module.exports = router;
