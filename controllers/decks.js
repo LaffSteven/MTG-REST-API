852c332207d22376bd8788ce8500bd081cd9b6c6
LaffSteven <steve.laff@gmail.com>
2022-06-01 11:31:17 -0400

    created post route

--- controllers/decks.js
+++ controllers/decks.js
@@ -4,7 +4,18 @@ const cors = require('cors')
 const Deck = require('../models/deck.js')
 
 router.get('/', (req, res) => {
-    res.send('Deck Index Page')
+    Deck.find({}, (err, foundDecks) = >{
+        res.json(foundDecks)
+    })
+})
+
+router.post('/', (req, res) => {
+    Deck.create(req.body, (err, createdDeck) => {
+        if (err) {
+            console.log(err.message);
+        }
+        console.log(`Created new deck: ${createdDeck.name}`);
+    })
 })
 
 module.exports = router;
